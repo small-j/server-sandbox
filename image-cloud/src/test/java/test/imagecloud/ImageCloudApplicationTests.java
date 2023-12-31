22940b001f66a69542c144b2de91c9e879d114d9
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2023-12-31 17:45:20 +0900

    init: 프로젝트 구성

---
+++ image-cloud/src/test/java/test/imagecloud/ImageCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package test.imagecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
