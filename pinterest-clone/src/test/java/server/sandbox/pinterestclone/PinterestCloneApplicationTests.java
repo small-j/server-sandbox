4469d5b3273701078bdd4aea59fd8c51f504208b
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-03-21 16:50:47 +0900

    init pinterest clone project

---
+++ pinterest-clone/src/test/java/server/sandbox/pinterestclone/PinterestCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package server.sandbox.pinterestclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinterestCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
