8ace3d6d4372dd706df1181cc3a8a453083dcc51
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2024-04-26 17:04:51 +0900

    feat : login 테스트 추가

--- pinterest-clone/src/test/java/server/sandbox/pinterestclone/service/UserServiceTest.java
+++ pinterest-clone/src/test/java/server/sandbox/pinterestclone/service/UserServiceTest.java
@@ -4,12 +4,15 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.transaction.annotation.Transactional;
 import server.sandbox.pinterestclone.domain.Image;
 import server.sandbox.pinterestclone.domain.User;
+import server.sandbox.pinterestclone.domain.dto.LoginInfoRequest;
 import server.sandbox.pinterestclone.domain.dto.SaveImageRequest;
 import server.sandbox.pinterestclone.domain.dto.UserInfoResponse;
 import server.sandbox.pinterestclone.domain.dto.UserRequest;
+import server.sandbox.pinterestclone.jwt.dto.JwtTokenHeaderForm;
 import server.sandbox.pinterestclone.repository.ImageRepository;
 import server.sandbox.pinterestclone.repository.UserRepository;
 
@@ -41,6 +44,24 @@ class UserServiceTest {
         int id = userService.register(userRequest);
 
         Assertions.assertThat(userRepository.findById(id)).isNotNull();
+    }
+
+    @Test
+    void login() {
+        String password = "1234";
+        UserRequest userRequest = UserRequest.builder()
+                .email("smallj@gmail.com")
+                .name("jiyun")
+                .password(password)
+                .build();
+        userService.register(userRequest);
+
+        LoginInfoRequest loginInfoRequest = new LoginInfoRequest(userRequest.getEmail(), password);
+        LoginInfoRequest wrongLoginInfo = new LoginInfoRequest(userRequest.getEmail(), "");
+        JwtTokenHeaderForm jwtTokenHeaderForm = userService.login(loginInfoRequest);
+
+        Assertions.assertThat(jwtTokenHeaderForm.getJwtToken()).isNotNull();
+        org.junit.jupiter.api.Assertions.assertThrows(BadCredentialsException.class, () -> userService.login(wrongLoginInfo));
     }
 
     @Test
